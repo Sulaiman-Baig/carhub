67fb75d663e26c0daae4f3f28377d859bd9f13f7
Sulaiman-Baig <engrsulaimanbaig@gmail.com>
2023-10-03 23:54:06 +0500

    hero completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,14 @@ const Hero = () => {
                     containerStyles='bg-primary-blue text-white rounded-full mt-10 p-3'
                     handleClick={handleScroll }
                 />
+            </div>
+            <div className='hero__image-container'>
+                <div className='hero__image'>
+                    <Image src='/hero.png' alt='hero' fill className='object-contain'/>
+                </div>
+                <div className='hero__image-overlay'>
 
+                </div>
             </div>
         </div>
     )
